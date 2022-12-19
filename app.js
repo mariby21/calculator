0a77337fe99d7ce03b52d1db09f4921c9859b27b
mariby21 <phenal21@gmail.com>
2022-12-19 12:41:15 +0200

    Calculator

---
+++ app.js
@@ -0,0 +1,26 @@
+(function () {
+  let inputField = document.querySelector('.input-field ');
+  let buttons = document.querySelectorAll('.btn');
+  let btnClear = document.querySelector('.btn-clear');
+  let btnEqual = document.querySelector('.btn-equals');
+
+  buttons.forEach(function (b) {
+    b.addEventListener('click', function (e) {
+      let target = e.target.dataset.num;
+      inputField.value += target;
+    });
+  });
+
+  btnEqual.addEventListener('click', function (e) {
+    if (inputField.value === '') {
+      inputField.value = 'Please Enter';
+    } else {
+      let sum = eval(inputField.value);
+      inputField.value = sum;
+    }
+  });
+
+  btnClear.addEventListener('click', function (e) {
+    inputField.value = '';
+  });
+})();
